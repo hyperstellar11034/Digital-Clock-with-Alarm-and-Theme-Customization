37c7c388a69c7717510ca034aaa517f049e1cb6b
MirAi <143268708+hyperstellar11034@users.noreply.github.com>
2024-12-25 02:33:20 +0330

    updated the textbook-style cout message
    A thought struck me: this is "my" project; my creation. I can bring my thoughts into them, unrestricted.
    So I went ahead and revised the textbook 𝘤𝘰𝘶𝘵 message. 
    Choosing not to embrace your freedom, even in moments where you can act as you wish, feels concerning.
    Fear of flying... let’s not live like that.

--- Main.cpp
+++ Main.cpp
@@ -38,7 +38,7 @@ void displayClock(int alarmHour, int alarmMinute, const Theme& theme) {
         resetColor();
 
         if (localTime.tm_hour == alarmHour && localTime.tm_min == alarmMinute) {
-            cout << "\nAlarm ringing!" << endl;
+            cout << "\nAlarm ringing..." << endl;
             playAlarmSound(4, 1); // Play beep 3 times with 1-second intervals
             break; // Exit the loop
         }
@@ -51,7 +51,12 @@ int main() {
     int alarmHour, alarmMinute;
     Theme theme = {"36", "44"}; // Customizable theme colors
 
-    cout << "Enter alarm time (HH MM): ";
+    cout << "\"Time's an Illusion\"\nA Mesmerizing Concept.\nBut we still use dates in such boring ways...\n"
+    "Weeks, Months and Years. Even \"A Day\" is Manipulatuve;\nThinking that we get to build a"
+    " good day tomorrow, is an illusion of a second chance.\nWho gets a \"second\" chance?\nUs Shameless"
+    " People. Asking for nth chance.\nAnd we're given that! Don't we forget again?\nDon't You..?"
+    "\nEvery Moment is Magical and Unique.\nand you're still busy working on distractions."
+    "\ngo ahead and set your alarm...\nEnter alarm time (HH MM): ";
     cin >> alarmHour >> alarmMinute;
 
     cout << "Digital Clock with Alarm - Press Ctrl+C to stop\n";
